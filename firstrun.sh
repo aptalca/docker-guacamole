ac0e0643290316de9bf0181cd3c58586bedf42dd
Jason Bean <taddeusz@gmail.com>
2017-02-07 09:29:16 -0600

    Keep mysql extension in classpath folder. Move ldap-schema folder to /config/guacamole.

--- firstrun.sh
+++ firstrun.sh
@@ -6,7 +6,8 @@ if [ -f /config/guacamole/guacamole.properties ]; then
 else
   echo "Creating properties from template."
   mkdir -p /config/databases /config/guacamole/extensions
-  mv /var/lib/guacamole/classpath/*auth*.jar /config/guacamole/extensions
+  mv /var/lib/guacamole/classpath/*ldap*.jar /config/guacamole/extensions
+  mv -R /var/lib/guacamole/ldap-schema /config/guacamole
   cp -R /etc/firstrun/. /config/guacamole
   PW=$(pwgen -1snc 32)
   sed -i -e 's/some_password/'$PW'/g' /config/guacamole/guacamole.properties
