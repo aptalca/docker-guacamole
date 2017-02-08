5ded420024c51cc929e996788dc8279c90c5b01f
Jason Bean <taddeusz@gmail.com>
2017-02-08 09:46:05 -0600

    Use environment variables to add or remove extensions.

--- firstrun.sh
+++ firstrun.sh
@@ -12,40 +12,57 @@ else
 fi
 
 # Check if extensions files exists. Copy or upgrade if necessary.
-if [ -e /config/guacamole/extensions/*ldap*.jar ]; then
-  oldLDAPFiles=( "/config/guacamole/extensions/*ldap*.jar" )
-  newLDAPFiles=( "/var/lib/guacamole/extensions/*ldap*.jar" )
+OPTLDAP=${OPT_LDAP^^}
+if [ $OPTLDAP = "Y" ]; then
+  if [ -e /config/guacamole/extensions/*ldap*.jar ]; then
+    oldLDAPFiles=( "/config/guacamole/extensions/*ldap*.jar" )
+    newLDAPFiles=( "/var/lib/guacamole/extensions/*ldap*.jar" )
 
-  if diff ${oldLDAPFiles[0]} ${newLDAPFiles[0]} >/dev/null ; then
-  	echo "Using existing LDAP extension."
+    if diff ${oldLDAPFiles[0]} ${newLDAPFiles[0]} >/dev/null ; then
+    	echo "Using existing LDAP extension."
+    else
+    	echo "Upgrading LDAP extension."
+    	rm /config/guacamole/extensions/*ldap*.jar
+    	rm -R /config/ldap-schema
+    	cp /var/lib/guacamole/extensions/*ldap*.jar /config/guacamole/extensions
+    	cp -R /var/lib/guacamole/ldap-schema /config
+    fi
   else
-  	echo "Upgrading LDAP extension."
-  	rm /config/guacamole/extensions/*ldap*.jar
-  	rm -R /config/ldap-schema
-  	cp /var/lib/guacamole/extensions/*ldap*.jar /config/guacamole/extensions
-  	cp -R /var/lib/guacamole/ldap-schema /config
+    echo "Copying LDAP extension."
+    cp /var/lib/guacamole/extensions/*ldap*.jar /config/guacamole/extensions
+    cp -R /var/lib/guacamole/ldap-schema /config
   fi
-else
-  echo "Copying LDAP extension."
-  cp /var/lib/guacamole/extensions/*ldap*.jar /config/guacamole/extensions
-  cp -R /var/lib/guacamole/ldap-schema /config
+elif [ $OPTLDAP = "N" ]; then
+  if [ -e /config/guacamole/extensions/*ldap*.jar ]; then
+    echo "Removing LDAP extension."
+    rm /config/guacamole/extensions/*ldap*.jar
+    rm -R /config/ldap-schema
+  fi
 fi
 
-#if [ -e /config/guacamole/extensions/*duo*.jar ]; then
-#  oldDuoFiles=( "/config/guacamole/extensions/*duo*.jar" )
-#  newDuoFiles=( "/var/lib/guacamole/extensions/*duo*.jar" )
-#
-#  if diff ${oldDuoFiles[0]} ${newDuoFiles[0]} >/dev/null ; then
-#    echo "Using existing Duo extension."
-#  else
-#    echo "Upgrading Duo extension."
-#    rm /config/guacamole/extensions/*duo*.jar
-#    cp /var/lib/guacamole/extensions/*duo*.jar /config/guacamole/extensions
-#  fi
-#else
-#  echo "Copying Duo extension."
-#  cp /var/lib/guacamole/extensions/*duo*.jar /config/guacamole/extensions
-#fi
+OPTDUO=${OPT_DUO^^}
+if [ $OPTDUO = "Y" ]; then
+  if [ -e /config/guacamole/extensions/*duo*.jar ]; then
+    oldDuoFiles=( "/config/guacamole/extensions/*duo*.jar" )
+    newDuoFiles=( "/var/lib/guacamole/extensions/*duo*.jar" )
+
+    if diff ${oldDuoFiles[0]} ${newDuoFiles[0]} >/dev/null ; then
+      echo "Using existing Duo extension."
+    else
+      echo "Upgrading Duo extension."
+      rm /config/guacamole/extensions/*duo*.jar
+      cp /var/lib/guacamole/extensions/*duo*.jar /config/guacamole/extensions
+    fi
+  else
+    echo "Copying Duo extension."
+    cp /var/lib/guacamole/extensions/*duo*.jar /config/guacamole/extensions
+  fi
+elif [ $OPTDUO = "N" ]; then
+  if [ -e /config/guacamole/extensions/*duo*.jar ]; then
+    echo "Removing Duo extension."
+    rm /config/guacamole/extensions/*duo*.jar
+  fi
+fi
 
 ln -s /config/guacamole /usr/share/tomcat7/.guacamole
 chown nobody:users -R /config/
